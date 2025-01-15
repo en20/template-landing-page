13c19cb94d51f3e1bf721dccc3d014fcc7f536e5
en20 <enzolozano90@gmail.com>
2025-01-15 15:54:16 -0300

    fix: change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
     <div className="footer-wrapper">
       <div className="footer-section-one">
         <div className="footer-logo-container">
-          <img src={Logo} alt="" />
+          <h1>PAGEFY</h1>
         </div>
         <div className="footer-icons">
           <BsTwitter />
